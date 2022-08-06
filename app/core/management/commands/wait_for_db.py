219038b6e79ca45df67086685de3e85ff2083acc
Ridealist <cjsn92@gmail.com>
2022-08-07 00:17:30 +0900

    chore: annotate unused import by flake8.

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -2,8 +2,7 @@ import time
 
 from psycopg2 import OperationalError as Psycopg2_OpError
 
-from django.db import connections
-
+# from django.db import connections
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
